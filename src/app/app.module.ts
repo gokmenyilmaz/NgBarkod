2c8235eb764f78f19d627b916c2c2f4646d4e44e
gokmen.yilmaz <gokmen1977@gmail.com>
2020-12-28 05:03:01 +0300

    uu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ZXingScannerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
